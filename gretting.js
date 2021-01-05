345591845c0c94b19d5aa48296672b2494f4c4d0
yujin mac <kimyu28jin@gmail.com>
2021-01-05 13:43:24 +0900

    처음 접속시 todoInput 숨기기 추가

--- gretting.js
+++ gretting.js
@@ -1,9 +1,11 @@
 const form = document.querySelector(".js-form");
 const input = form.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
+const todoForm = document.querySelector(".js-toDoForm");
 
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
+const HIDE = "hide";
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
@@ -18,12 +20,14 @@ function handleSubmit(event) {
 
 function askForName() {
   form.classList.add(SHOWING_CN);
+  todoForm.classList.add(HIDE);
   form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN); //텍스트를 색칠할거라면 폼을 숨겨
   greeting.classList.add(SHOWING_CN);
+  todoForm.classList.remove("hide");
   greeting.innerText = `반가워! ${text}`;
 }
 
@@ -32,6 +36,7 @@ function loadName() {
   if (currentUser === null) {
     askForName();
   } else {
+    todoForm.classList.remove("hide");
     paintGreeting(currentUser);
   }
 }
